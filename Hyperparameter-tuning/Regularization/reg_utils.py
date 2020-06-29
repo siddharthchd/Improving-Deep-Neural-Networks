bbc69f7bec233c1cf9722c64979f7615dd505c80
siddharthchd <siddharth.chaudhary1705@gmail.com>
2020-06-29 23:55:04 +0530

    update

--- Hyperparameter-tuning/Regularization/reg_utils.py
+++ Hyperparameter-tuning/Regularization/reg_utils.py
@@ -36,3 +36,61 @@ def initialize_parameters(layers_dims):
         assert(parameters['b' + str(b)].shape == (layers_dims[l], 1))
 
     return parameters
+
+
+def forward_propagation(X, parameters):
+
+    # retrieve parameters
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+    W3 = parameters['W3']
+    b3 = parameters['b3']
+
+    # LINEAR -> RELU -> LINEAR -> RELU -> LINEAR ->SIGMOID
+    Z1 = np.dot(W1, X) + b1
+    A1 = relu(Z1)
+    Z2 = np.dot(W2, A1) + b2
+    A2 = relu(Z2)
+    Z3 = np.dot(W3, A2) + b3
+    A3 = sigmoid(Z3)
+
+    cache = (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3)
+
+    return A3, cache
+
+
+def backward_propagation(X, Y, cache):
+
+    m = X.shape[1]
+    (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3) = cache
+
+    dZ3 = A3 - Y
+    dW3 = 1 / m * np.dot(dZ3, A2.T)
+    db3 = 1 / m * np.sum(dZ3, axis=1, keepdims=True)
+
+    dA2 = np.dot(W3.T, dZ3)
+    dZ2 = np.multiply(dA2, np.int64(A2 > 0))
+    dW2 = 1 / m * np.dot(dZ2, A1.T)
+    db1 = 1 / m * np.sum(dZ2, axis=1, keepdims=True)
+
+    dA1 = np.dot(W2.T, dZ2)
+    dZ1 = np.multiply(dA1, np.int64(A1 > 0))
+    dW2 = 1 / m * np.dot(dZ1, X.T)
+    db1 = 1 / m * np.sum(dZ1, axis=1, keepdims=True)
+
+    gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3
+                 "dA2": dA2, "dZ3": dZ2, "dW3": dW2, "db2": db2,
+                 "dA2": dA1, "dZ3": dZ1, "dW3": dW1, "db1": db1}
+
+    return gradients
+
+
+def update_parameters(parameters, grads, learning_rate):
+
+    n = len(parameters) // 2    # number of layers in the neural networks
+
+    for l in range(1, L):
+
+        parameters['W' + str()]
